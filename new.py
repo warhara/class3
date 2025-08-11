0d1dde0ae4f719e803ecfaa80b13dc5ab67b5603
warhara <warhar121@hotmail.com>
2025-08-11 17:56:16 +0100

    new py file add to another to offline

---
+++ new.py
@@ -0,0 +1,13 @@
+import random
+
+messages = [
+    "You’ve got this!",
+    "Keep going — you’re doing great!",
+    "One step at a time.",
+    "Mistakes mean you’re learning.",
+    "Your future self will thank you."
+]
+
+print(random.choice(messages))
+
+
